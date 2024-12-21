cf74ec6dbd5cd921e7c0c924ce8ed98637fcb017
Jonny Muir <jonnypmuir@gmail.com>
2024-12-21 10:15:03 +0000

    feat: solve days 4

--- day4/src/board.rs
+++ day4/src/board.rs
@@ -18,55 +18,82 @@ impl Board {
         }
     }
 
-    pub fn solve(&self, word: &str) -> u32 {
-        self.coordinates()
-            .map(|coord| self.wordcount(word, coord))
-            .sum()
+    pub fn solve(&self, word: &str) -> usize {
+        // We create an iterator of all direction transforms (e.g. 0,1 = right, -1,-1 = diagonal up and left etc )
+        let directions = iproduct!(-1..2, -1..2);
+
+        // Then for each coordinate for each direction - look for the word. 
+        // This could be optimised as it means checking all directions even though the letter doesn't match
+        iproduct!(self.coordinates(), directions)
+            .filter(|(coord, direction)| self.is_word_at_coord(word, *coord, *direction))
+            .count()
     }
 
-    fn wordcount(&self, word: &str, coord: (usize, usize)) -> u32 {
+    pub fn solve_xmas(&self) -> usize {
+        // To solve X-MAS, we iterate round the board, looking for an A that has diagnal pairs of M and S's surrounding them
+        self.coordinates()
+            .filter(|(x, y)| {
+                if *x > 0
+                    && *x < self.width - 1
+                    && *y > 0
+                    && *y < self.height - 1
+                    && self.board[*y][*x] == 'A'
+                {
+                    let pairs = [
+                        ((x - 1, y - 1), (x + 1, y + 1)),
+                        ((x + 1, y + 1), (x - 1, y - 1)),
+                        ((x + 1, y - 1), (x - 1, y + 1)),
+                        ((x - 1, y + 1), (x + 1, y - 1)),
+                    ];
+
+                    let matches = pairs.iter().filter(|&(p1, p2)| {
+                        self.board[p1.1][p1.0] == 'M' && self.board[p2.1][p2.0] == 'S'
+                    }).count();
+
+                    return matches >= 2;
+                }
+                false
+            })
+            .count()
+    }
+
+    fn is_word_at_coord(&self, word: &str, coord: (usize, usize), direction: (i32, i32)) -> bool {
+        println!(
+            "Testing {:?} for {} in direction {:?}",
+            coord,
+            word.chars().next().unwrap(),
+            direction
+        );
         if self.board[coord.1][coord.0] == word.chars().next().unwrap() {
+            println!("Found");
+
+            // Are we the end of the word? If so return a match
             if word.len() == 1 {
-                return 1;
-            } else {
-                return self
-                    .adjacent_coords(coord)
-                    .iter()
-                    .map(|coord| self.wordcount(&word[1..], *coord))
-                    .sum::<u32>();
+                println!("Found word finishing at {:?}", coord);
+                return true;
+            }
+
+            // Otherwise transform the coord by the direction
+            let (new_x, new_y) = (
+                (coord.0 as i32 + direction.0),
+                (coord.1 as i32 + direction.1),
+            );
+
+            if new_x >= 0 && new_x < self.width as i32 && new_y >= 0 && new_y < self.height as i32 {
+                // The new coordinate is within bounds, so use it
+                return self.is_word_at_coord(
+                    &word[1..],
+                    (new_x as usize, new_y as usize),
+                    direction,
+                );
             }
         }
-
-        0
+        println!("Not found");
+        false
     }
 
     fn coordinates(&self) -> impl Iterator<Item = (usize, usize)> {
         iproduct!(0..self.width, 0..self.height)
-    }
-
-    fn adjacent_coords(&self, coord: (usize, usize)) -> Vec<(usize, usize)> {
-        let directions = [
-            (0, 1),   // Right
-            (0, -1),  // Left
-            (1, 0),   // Down
-            (-1, 0),  // Up
-            (1, 1),   // Diagonal Down-Right
-            (-1, -1), // Diagonal Up-Left
-            (1, -1),  // Diagonal Down-Left
-            (-1, 1),  // Diagonal Up-Right
-        ];
-
-        directions
-            .iter()
-            .map(|(dx, dy)| (coord.0 as i32 + dx, coord.1 as i32 + dy))
-            .filter_map(|(x, y)| {
-                if x >= 0 && y >= 0 && x < self.width as i32 && y < self.height as i32 {
-                    Some((x as usize, y as usize))
-                } else {
-                    None
-                }
-            })
-            .collect()
     }
 }
 
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,24 +3,31 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 mod board;
 
-
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        eprintln!("Usage: {} <filename>", args[0]);
+    if args.len() < 2 || args.len() > 3 {
+        eprintln!("Usage for word solution: {} <filename> <word>", args[0]);
+        eprintln!("e.g: {} input.txt XMAS", args[0]);
+        eprintln!("Usage for X-MAS solution: {} <filename>", args[0]);
+        eprintln!("e.g: {} input.txt", args[0]);
         std::process::exit(1);
     }
 
     let file = File::open(&args[1])?;
     let reader = BufReader::new(file);
 
-    let board:board::Board = board::Board::new(reader
-        .lines()
-        .map(|line| line.unwrap().chars().collect::<Vec<char>>())
-        .collect());
-        
-    println!("Word count: {}", board.solve("XMAS"));
- 
+    let board: board::Board = board::Board::new(
+        reader
+            .lines()
+            .map(|line| line.unwrap().chars().collect::<Vec<char>>())
+            .collect(),
+    );
+
+    if args.len() == 3 {
+        println!("Word count: {}", board.solve(&args[2]));
+    } else {
+        println!("X-mas count: {}", board.solve_xmas());
+    }
     Ok(())
 }
